fc57a5319ee788783c9e6f1b929062c75d7aaedd
hzhang0311 <hanwen9803@gmail.com>
2024-11-22 00:06:27 -0600

    Practice

--- HelloWorld.R
+++ HelloWorld.R
@@ -1,5 +1,13 @@
-print("hello world!!!")
-print("Ni Hao")
-x <- 1
-b <- 2
-c <- a + b
+n_treat <- 84277
+n_untreat <- 6508019
+
+treat <- c(59135, 1010, 5234, 18898)
+untreat <- c(4717678, 72675, 442638, 1275028)
+
+prop_trt <- round(treat/n_treat * 100, 1)
+prop_untrt <- round(untreat/n_untreat * 100, 1)
+
+prop_trt
+prop_untrt
+sum(prop_trt)
+sum(prop_untrt)
